0535a2231d293bf75ab11aab7c87305a8a3d4f64
Ilya Rubnich <irubnich@gmail.com>
2013-07-21 03:04:33 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CCNY Scheduler
+This is a simple web app for building and sharing schedules for CCNY students.
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CcnyScheduler::Application.config.session_store :cookie_store, key: '_ccny_scheduler_session'
